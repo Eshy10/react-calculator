5af9a4b118a95657daa79e434b4a58b979dfbc26
Eshy10 <adewalebose15@gmail.com>
2020-09-15 15:00:16 +0100

    change app component to a stateful component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Display from '../display/Display';
 import ButtonPanel from '../buttonPanel/ButtonPanel';
 import styles from './App.module.css';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../../logic/calculate';
 
-function App() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.App}>
-        <h1 className={styles.title}>Calculator App</h1>
-        <Display />
-        <ButtonPanel />
+class App extends Component() {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.App}>
+          <h1 className={styles.title}>Calculator App</h1>
+          <Display />
+          <ButtonPanel />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
