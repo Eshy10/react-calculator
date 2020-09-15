f6f15f7d73efcdc06950018497b4458d5c688318
Eshy10 <adewalebose15@gmail.com>
2020-09-15 13:42:12 +0100

    modify calculate method

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,9 +1,28 @@
-const calculate = (name, ...data) => {
-  const dataObject = data;
-  if (name === '+/-') {
-    dataObject.total *= -1;
-    dataObject.next *= -1;
+import operate from './operate';
+
+const calculate = (data, buttonName) => {
+  let { total, next, operation } = data;
+  const operators = ['+', '-', '÷', 'X', '%'];
+
+  if (buttonName === 'AC') {
+    total = 0;
+    next = null;
+    operation = null;
+  } else if (buttonName === '+/-') {
+    total *= -1;
+    next *= -1;
+  } else if (operators.includes(buttonName)) {
+    operate(total, next, operation);
+  } else if ((buttonName === '=') && (next && total)) {
+    total = operate(total, next, operation);
+    next = null;
+    operation = null;
+  } else if (buttonName === '.' && next) {
+    if (!next.includes('.')) {
+      next += '.';
+    }
   }
+  return { total, next, operation };
 };
 
 export default calculate;
